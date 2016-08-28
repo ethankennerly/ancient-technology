90f2081ab20614b8cc3efb4ba7b86131c6be5abd
Ethan Kennerly <ethan@lumoslabs.com>
2016-08-27 19:37:23 -0700

    Unit test setup building cells.

---
+++ Assets/Editor/TestBuildingModel.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Finegamedesign.Utils;
+
+namespace Finegamedesign.CityOfWords
+{
+	public sealed class TestBuildingModel
+	{
+		[Test]
+		public void SetupCellCount()
+		{
+			var model = new BuildingModel();
+			model.cellCount = 2;
+			model.Setup();
+			Assert.AreEqual("available", model.cellStates[0]);
+			Assert.AreEqual("none", model.cellStates[1]);
+		}
+	}
+}
---
+++ Assets/Scripts/BuildingModel.cs
@@ -0,0 +1,19 @@
+using Finegamedesign.Utils;
+
+namespace Finegamedesign.CityOfWords
+{
+	public sealed class BuildingModel
+	{
+		public string[] cellStates;
+		public int cellCount = 0;
+
+		public void Setup()
+		{
+			cellStates = new string[cellCount];
+			for (int index = 0; index < cellCount; index++)
+			{
+				cellStates[index] = index <= 0 ? "available" : "none";
+			}
+		}
+	}
+}
